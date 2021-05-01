3ce55ee26fd0f72383f0925307ad2e1b35ffb0fa
ASUS <ASUS@LAPTOP-9PJNQ24C>
2021-05-02 07:41:47 +0800

    fff

--- Exercise 4.3/src/Main.java
+++ Exercise 4.3/src/Main.java
@@ -4,5 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		Orange a=new Orange("Orange");
 		System.out.println(a);
+		
+		Apple b=new Apple("Apple");
+		System.out.println(b);
 	}
 }
